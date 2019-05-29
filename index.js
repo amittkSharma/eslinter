9475204001215d74f6c949f6d79c0259376e8797
Amitt K Sharma <amit12.sharma@gmail.com>
2019-05-29 16:40:46 +0200

    Updating the non inclusive rules
    Updating rules

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ module.exports = {
   root: true,
   extends: ['react-app', 'eslint:recommended'],
   rules: {
-    'no-console': 'error',
+    'default-case': 'error',
+    'max-classes-per-file': ['error', 1],
+    'max-len': ['error', { code: 120, tabWidth: 4 }],
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'no-empty-function': 'error',
+    'no-eq-null': 'error',
   },
 }
